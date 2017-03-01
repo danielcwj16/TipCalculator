5729a9c2195009479d7be4b075ee4a6083acdd3d
danielcwj16 <weijiec@icloud.com>
2017-03-01 00:10:59 -0500

    Version 1.0

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("I can't wait to pusht this project to github!")
     }
 
     override func didReceiveMemoryWarning() {
